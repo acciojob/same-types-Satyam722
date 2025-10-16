268fedb134bde55a7daae9886c04990f2cb90a2e
Satyam <nikamsatyam187@gmail.com>
2025-10-16 16:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function isSameType(value1, value2) {
-   if (isNaN(value1) && isNaN(value2)) {
+   if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
 
   if (typeof value1 === typeof value2) {
-    alert(true);
+       return true;
 } else {
-    alert(false);
+    return false;
 }
 
 
